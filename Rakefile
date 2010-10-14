6c462b553ee09d06c1952057cf404e3b53ab4f36
Philip Hallstrom <philip@pjkh.com>
2010-10-14 13:55:53 -0700

    Initial commit

---
+++ Rakefile
@@ -0,0 +1,10 @@
+# -*- ruby -*-
+
+require 'rubygems'
+require 'hoe'
+
+Hoe.spec 'el_finder' do
+  developer('Philip Hallstrom', 'philip@pjkh.com')
+end
+
+# vim: syntax=ruby
---
+++ test/test_el_finder.rb
@@ -0,0 +1,8 @@
+require "test/unit"
+require "el_finder"
+
+class TestElFinder < Test::Unit::TestCase
+  def test_sanity
+    flunk "write tests or I will kneecap you"
+  end
+end
