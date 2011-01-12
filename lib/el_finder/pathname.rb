51cb73b6597614fdabdf93462570b1ab4a04ec76
Philip Hallstrom <philip@pjkh.com>
2011-01-12 14:03:51 -0800

    completely redone ElFinder::Pathname

--- lib/el_finder/pathname.rb
+++ lib/el_finder/pathname.rb
@@ -3,75 +3,195 @@ require 'pathname'
 
 module ElFinder
 
-  class Pathname < ::Pathname
+  class Pathname
+    attr_reader :root, :path
 
+    #
+    def initialize(root, path = '')
+      @root = root.is_a?(Pathname) ? root : ::Pathname.new(root)
+
+      @path = ::Pathname.new(path) 
+      @path = path.is_a?(Pathname) ? path : ::Pathname.new(path)
+      if absolute?
+        if @path.cleanpath.to_s.start_with?(@root.to_s)
+          @path = ::Pathname.new @path.to_s.slice((@root.to_s.length + 1)..-1)
+        elsif @path.cleanpath.to_s.start_with?(@root.realpath.to_s)
+          @path = ::Pathname.new @path.to_s.slice((@root.realpath.to_s.length + 1)..-1)
+        else
+          raise SecurityError, "Absolute paths are not allowed" 
+        end
+      end
+      raise SecurityError, "Paths outside the root are not allowed" if outside_of_root?
+
+    end # of initialize
+    
     #
     def +(other)
-      self.class.new(super(other).to_s)
-    end
-
-    #
-    def join(*args)
-      self.class.new(super(*args).to_s)
-    end
-
-    #
-    def rename(to)
-      super(to)
-    rescue Errno::EXDEV
-      FileUtils.move(self.to_s, to.to_s)
-      @path = to.to_s
-    end # of rename
-
-    #
-    def self.new_with_root(root, path = '')
-      path = path.to_s.sub(root, '') if path.to_s[0,1] == '/'
-      new(superclass.new(File.join(root, path)).cleanpath.to_s)
-    end # of self.new_with_root
-
-    #
-    def basename_without_extension
-      basename(extname)
-    end
-
-    #
-    def duplicate
-      _dirname = dirname
-      _extname = extname
-      _basename = basename(_extname)
-      copy = 0
-      if _basename.to_s =~ /^(.*) copy (\d+)$/
-        _basename = $1
-        copy = $2.to_i
+      if other.is_a? ::ElFinder::Pathname
+        other = other.path
       end
+      self.class.new(@root, (@path + other).to_s)
+    end # of +
 
-      begin
-        copy += 1
-        new_file = self.class.superclass.new(_dirname + "#{_basename} copy #{copy}#{_extname}")
-      end while new_file.exist?
-      new_file
-    end # of duplicate
+    #
+    def is_root?
+      @path.to_s.empty?
+    end
 
+    #
+    def absolute?
+      @path.absolute?
+    end # of absolute?
+
+    #
+    def relative?
+      @path.relative?
+    end # of relative?
+
+    #
+    def outside_of_root?
+      !cleanpath.to_s.start_with?(@root.to_s)
+    end # of outside_of_root?
+
+    #
+    def fullpath
+      @path.nil? ? @root : @root + @path
+    end # of fullpath
+
+    #
+    def cleanpath
+      fullpath.cleanpath
+    end # of cleanpath
+
+    #
+    def realpath
+      fullpath.realpath
+    end # of realpath
+
+    #
+    def basename(*args)
+      @path.basename(*args)
+    end # of basename
+
+    #
+    def basename_sans_extension
+      @path.basename(@path.extname)
+    end # of basename
+
+    #
+    def basename(*args)
+      @path.basename(*args)
+    end # of basename
+
+    #
+    def dirname
+      @path.dirname
+    end # of basename
+
+    #
+    def extname
+      @path.nil? ? '' : @path.extname
+    end # of extname
+
+    #
+    def to_s
+      @path.to_s
+    end # of to_s
+
+    #
+    def children(with_directory=true)
+      realpath.children(with_directory).map{|e| self.class.new(@root, e)}
+    end
+
+    #
+    def touch(options = {})
+      FileUtils.touch(cleanpath, options)
+    end
+    
     #
     def unique
       return self.dup unless self.file?
-
-      _dirname = dirname
-      _extname = extname
-      _basename = basename(_extname)
-      copy = 0
-
+      copy = 1
       begin
+        new_file = self.class.new(@root, dirname + "#{basename_sans_extension} #{copy}#{extname}")
         copy += 1
-        new_file = self.class.superclass.new(_dirname + "#{_basename} #{copy}#{_extname}")
       end while new_file.exist?
       new_file
     end # of unique
 
     #
-    def relative_to(pathname)
-      self == pathname ? '' : relative_path_from(pathname).to_s
-    end # of relative_to
+    def duplicate
+      _basename = basename_sans_extension
+      copy = 1
+      if _basename.to_s =~ /^(.*) copy (\d+)$/
+        _basename = $1
+        copy = $2.to_i
+      end
+      begin
+        new_file = self.class.new(@root, dirname + "#{_basename} copy #{copy}#{extname}")
+        copy += 1
+      end while new_file.exist?
+      new_file
+    end # of duplicate
+
+    #
+    def rename(to)
+      to = self.class.new(@root, to.to_s)
+      realpath.rename(to.fullpath.to_s)
+    rescue Errno::EXDEV
+      FileUtils.move(realpath.to_s, to.fullpath.to_s)
+    ensure
+      @path = to.path
+    end # of rename
+
+    %w(
+      directory? 
+      exist? 
+      file? 
+      readable? 
+      writable?
+    ).each do |meth|
+      class_eval <<-METHOD, __FILE__, __LINE__ + 1
+        def #{meth}
+          realpath.#{meth}
+        rescue Errno::ENOENT
+          false
+        end
+      METHOD
+    end
+
+    %w(
+      mtime
+      unlink
+    ).each do |meth|
+      class_eval <<-METHOD, __FILE__, __LINE__ + 1
+        def #{meth}
+          realpath.#{meth}
+        end
+      METHOD
+    end
+
+    %w(
+      mkdir
+      read
+    ).each do |meth|
+      class_eval <<-METHOD, __FILE__, __LINE__ + 1
+        def #{meth}(*args)
+          fullpath.#{meth}(*args)
+        end
+      METHOD
+    end
+
+    %w(
+      open
+    ).each do |meth|
+      class_eval <<-METHOD, __FILE__, __LINE__ + 1
+        def #{meth}(*args, &block)
+          fullpath.#{meth}(*args, &block)
+        end
+      METHOD
+    end
+
 
   end # of class Pathname
 
--- test/el_finder/test_pathname.rb
+++ test/el_finder/test_pathname.rb
@@ -1,6 +1,7 @@
-require "test/unit"
-require "el_finder"
-require "fileutils"
+require 'test/unit'
+require 'el_finder'
+require 'fileutils'
+require 'pp'
 
 class TestPathname < Test::Unit::TestCase
 
@@ -16,105 +17,267 @@ class TestPathname < Test::Unit::TestCase
 
   ################################################################################
 
-  def test_paths_are_always_clean
-    assert_equal File.join(@vroot, 'foo/bar'), ElFinder::Pathname.new_with_root(@vroot, File.join('foo', '..', 'foo', 'bar')).to_s
-    assert_equal @vroot, ElFinder::Pathname.new_with_root(@vroot, '').to_s
-    assert_equal File.join(@vroot, 'foo/bar'), ElFinder::Pathname.new_with_root(@vroot, File.join('/foo', 'bar')).to_s
+  def test_new_fails_without_root
+    assert_raise ArgumentError do
+      ElFinder::Pathname.new()
+    end
   end
 
+  def test_new_okay_with_root
+    assert_nothing_raised do
+      ElFinder::Pathname.new(@vroot)
+    end
+  end
+
+  def test_new_okay_with_root_and_path
+    assert_nothing_raised do
+      ElFinder::Pathname.new(@vroot, 'foo.txt')
+    end
+  end
+
+  def test_instance_variables_are_set_correctly
+    p = ElFinder::Pathname.new(@vroot, 'foo.txt')
+    assert_equal @vroot, p.root.to_s
+    assert_equal 'foo.txt', p.path.to_s
+  end
+
+  def test_instance_variables_are_set_correctly_with_nil_path
+    p = ElFinder::Pathname.new(@vroot)
+    assert_equal @vroot, p.root.to_s
+    assert_equal '', p.path.to_s
+  end
+
+  def test_instance_variables_are_kind_of_pathname
+    p = ElFinder::Pathname.new(@vroot, 'foo.txt')
+    assert_kind_of ::Pathname, p.root
+    assert_kind_of ::Pathname, p.path
+  end
+
+  def test_attempt_to_break_out_of_root_using_relative_path
+    assert_raise SecurityError do
+      p = ElFinder::Pathname.new(@vroot, '../foo.txt')
+    end
+  end
+
+  def test_attempt_to_break_out_of_root_using_absolute_path
+    assert_raise SecurityError do
+      p = ElFinder::Pathname.new(@vroot, '/foo.txt')
+    end
+  end
+
+  def test_new_with_full_path_matching_root
+    p = ElFinder::Pathname.new(@vroot, "#{@vroot}/foo.txt")
+    assert_equal 'foo.txt', p.path.to_s
+  end
+
+  def test_new_with_full_path_matching_root_realpath
+    p = ElFinder::Pathname.new(@vroot, "#{Pathname.new(@vroot).realpath.to_s}/foo.txt")
+    assert_equal 'foo.txt', p.path.to_s
+  end
+
+  def test_plus_string
+    p = ElFinder::Pathname.new(@vroot, 'foo')
+    p1 = p + 'bar'
+    assert_equal 'foo/bar', p1.path.to_s
+  end
+
+  def test_plus_pathname
+    p = ElFinder::Pathname.new(@vroot, 'foo')
+    p1 = p + Pathname.new('bar')
+    assert_equal 'foo/bar', p1.path.to_s
+  end
+
+  def test_plus_elfinder_pathname
+    p = ElFinder::Pathname.new(@vroot, 'foo')
+    p1 = p + ElFinder::Pathname.new(@vroot, 'bar')
+    assert_equal 'foo/bar', p1.path.to_s
+  end
+
+  def test_fullpath
+    assert_equal "#{@vroot}/one/two/three.txt", ElFinder::Pathname.new(@vroot, 'one/two/three.txt').fullpath.to_s
+  end
+
+  def test_file?
+    assert_equal true, ElFinder::Pathname.new(@vroot, 'README.txt').file?, "README.txt should be a file"
+    assert_equal false, ElFinder::Pathname.new(@vroot, 'INVALID').file?, "INVALID should not be a file"
+    assert_equal false, ElFinder::Pathname.new(@vroot, 'foo').file?, "foo should not be a file (it's a directory)"
+  end
+
+  def test_directory?
+    assert_equal true, ElFinder::Pathname.new(@vroot, 'foo').directory?, "foo should be a directory"
+    assert_equal false, ElFinder::Pathname.new(@vroot, 'README.txt').directory?, "README.txt should not be a directory (it's a file)"
+    assert_equal false, ElFinder::Pathname.new(@vroot, 'INVALID').directory?, "INVALID should not be a directory"
+  end
+
+  def test_exist?
+    assert_equal true, ElFinder::Pathname.new(@vroot, 'foo').exist?, "foo should exist"
+    assert_equal true, ElFinder::Pathname.new(@vroot, 'README.txt').exist?, "README.txt should exist"
+    assert_equal false, ElFinder::Pathname.new(@vroot, 'INVALID').exist?, "INVALID should not exist"
+  end
+
+  def test_readable?
+    assert_equal true, ElFinder::Pathname.new(@vroot, 'foo').readable?, "foo should be readable"
+    assert_equal true, ElFinder::Pathname.new(@vroot, 'README.txt').readable?, "README.txt should be readable"
+    assert_equal false, ElFinder::Pathname.new(@vroot, 'INVALID').readable?, "INVALID should not be readable"
+  end
+
+  def test_writable?
+    assert_equal true, ElFinder::Pathname.new(@vroot, 'foo').writable?, "foo should be writable"
+    assert_equal true, ElFinder::Pathname.new(@vroot, 'README.txt').writable?, "README.txt should be writable"
+    assert_equal false, ElFinder::Pathname.new(@vroot, 'INVALID').writable?, "INVALID should not be writable"
+  end
+
+  def test_mtime
+    assert_equal File.new(File.join(@vroot, 'foo')).mtime, ElFinder::Pathname.new(@vroot, 'foo').mtime
+    assert_equal File.new(File.join(@vroot, 'README.txt')).mtime, ElFinder::Pathname.new(@vroot, 'README.txt').mtime
+    assert_raise Errno::ENOENT do
+      ElFinder::Pathname.new(@vroot, 'INVALID').mtime
+    end
+  end
+
+  def test_unlink
+    assert_equal true, ElFinder::Pathname.new(@vroot, 'README.txt').exist?
+    ElFinder::Pathname.new(@vroot, 'README.txt').unlink
+    assert_equal false, ElFinder::Pathname.new(@vroot, 'README.txt').exist?
+    assert_raise Errno::ENOENT do
+      ElFinder::Pathname.new(@vroot, 'INVALID').unlink
+    end
+    assert_raise Errno::ENOTEMPTY do
+      ElFinder::Pathname.new(@vroot, 'foo').unlink
+    end
+  end
+
+  def test_basename
+    assert_equal 'README.txt', ElFinder::Pathname.new(@vroot, 'README.txt').basename.to_s
+    assert_equal 'README', ElFinder::Pathname.new(@vroot, 'README.txt').basename('.txt').to_s
+    assert_equal 'tom.txt', ElFinder::Pathname.new(@vroot, 'foo/tom.txt').basename.to_s
+  end
+
+  def test_basename_sans_extension
+    assert_equal 'README', ElFinder::Pathname.new(@vroot, 'README.txt').basename_sans_extension.to_s
+    assert_equal 'tom', ElFinder::Pathname.new(@vroot, 'foo/tom.txt').basename_sans_extension.to_s
+  end
+
+  def test_dirname
+    assert_equal '.', ElFinder::Pathname.new(@vroot, 'README.txt').dirname.to_s
+    assert_equal 'foo', ElFinder::Pathname.new(@vroot, 'foo/tom.txt').dirname.to_s
+  end
+
+  def test_to_s
+    assert_equal 'README.txt', ElFinder::Pathname.new(@vroot, 'README.txt').to_s
+    assert_equal 'foo/tom.txt', ElFinder::Pathname.new(@vroot, 'foo/tom.txt').to_s
+  end
+
+  def test_children
+    children = ElFinder::Pathname.new(@vroot, 'foo').children
+    assert_equal 4, children.size
+    assert_equal %w[philip sam sandy tom], children.map{|e| e.basename_sans_extension.to_s}.sort
+  end
+
+  def test_mkdir
+    p = ElFinder::Pathname.new(@vroot, 'some-dir')
+    assert_equal false, p.directory?
+    p.mkdir
+    assert_equal true, p.directory?
+  end
+
+  def test_read
+    p = ElFinder::Pathname.new(@vroot, 'foo/philip.txt')
+    assert_equal File.read(File.join(@vroot, 'foo/philip.txt')), p.read
+  end
+
+  def test_touch
+    p = ElFinder::Pathname.new(@vroot, 'newfile')
+    p.touch
+    assert_equal true, p.file?
+  end
+
+  def test_open
+    p = ElFinder::Pathname.new(@vroot, 'newfile')
+    assert_equal false, p.file?
+    p.open('w') {|f| f.puts "new"}
+    assert_equal true, p.file?
+    assert_equal File.read(File.join(@vroot, 'newfile')), p.read
+  end
+
+  def test_unique_on_create
+    file = ElFinder::Pathname.new(@vroot, 'foo.txt')
+    assert_equal 'foo.txt', file.unique.basename.to_s
+  end
+
+  def test_unique_conflict
+    file = ElFinder::Pathname.new(@vroot, 'pjkh.png')
+    assert_equal 'pjkh 1.png', file.unique.basename.to_s
+  end
+
+  def test_unique_conflict_twice
+    ElFinder::Pathname.new(@vroot, 'pjkh 1.png').touch
+    file = ElFinder::Pathname.new(@vroot, 'pjkh.png')
+    assert_equal 'pjkh 2.png', file.unique.basename.to_s
+  end
+
+
   def test_duplication_without_extension
-    assert_equal File.join(@vroot, 'README copy 1'), ElFinder::Pathname.new_with_root(@vroot,'README').duplicate.to_s
+    assert_equal File.join(@vroot, 'README copy 1'), ElFinder::Pathname.new(@vroot,'README').duplicate.fullpath.to_s
   end
 
   def test_2nd_duplication_without_extension
     ::FileUtils.touch(File.join(@vroot, 'README copy 1'))
-    assert_equal File.join(@vroot, 'README copy 2'), ElFinder::Pathname.new_with_root(@vroot,'README').duplicate.to_s
+    assert_equal File.join(@vroot, 'README copy 2'), ElFinder::Pathname.new(@vroot,'README').duplicate.fullpath.to_s
   end
 
   def test_duplication_with_extension
-    assert_equal File.join(@vroot, 'README copy 1.txt'), ElFinder::Pathname.new_with_root(@vroot,'README.txt').duplicate.to_s
+    assert_equal File.join(@vroot, 'README copy 1.txt'), ElFinder::Pathname.new(@vroot,'README.txt').duplicate.fullpath.to_s
   end
 
   def test_2nd_duplication_with_extension
     ::FileUtils.touch(File.join(@vroot, 'README copy 1.txt'))
-    assert_equal File.join(@vroot, 'README copy 2.txt'), ElFinder::Pathname.new_with_root(@vroot,'README.txt').duplicate.to_s
+    assert_equal File.join(@vroot, 'README copy 2.txt'), ElFinder::Pathname.new(@vroot,'README.txt').duplicate.fullpath.to_s
   end
 
   def test_duplication_of_duplication_lookalike
-    assert_equal File.join(@vroot, 'README copy A copy 1.txt'), ElFinder::Pathname.new_with_root(@vroot,'README copy A.txt').duplicate.to_s
+    assert_equal File.join(@vroot, 'README copy A copy 1.txt'), ElFinder::Pathname.new(@vroot,'README copy A.txt').duplicate.fullpath.to_s
   end
 
   def test_duplication_of_duplication_lookalike2
-    assert_equal File.join(@vroot, 'README copy copy 1.txt'), ElFinder::Pathname.new_with_root(@vroot,'README copy.txt').duplicate.to_s
+    assert_equal File.join(@vroot, 'README copy copy 1.txt'), ElFinder::Pathname.new(@vroot,'README copy.txt').duplicate.fullpath.to_s
   end
 
   def test_on_disk_duplication
-    file = ElFinder::Pathname.new_with_root(@vroot, 'README.txt')
-    FileUtils.touch(file)
+    file = ElFinder::Pathname.new(@vroot, 'README.txt')
+    file.touch
     assert_equal true, File.exist?(File.join(@vroot, 'README.txt'))
     duplicate = file.duplicate
-    FileUtils.touch(duplicate)
+    duplicate.touch
     assert_equal true, File.exist?(File.join(@vroot, 'README copy 1.txt'))
   end
 
-  ################################################################################
-
   def test_rename_on_same_filesystem
-    file = ElFinder::Pathname.new_with_root(@vroot, 'old.txt')
-    FileUtils.touch(file)
+    file = ElFinder::Pathname.new(@vroot, 'old.txt')
+    file.touch
     assert_equal true, File.exist?(File.join(@vroot, 'old.txt'))
     file.rename(File.join(@vroot, 'new.txt'))
     assert_equal false, File.exist?(File.join(@vroot, 'old.txt'))
     assert_equal true, File.exist?(File.join(@vroot, 'new.txt'))
+    assert_equal 'new.txt', file.path.to_s
   end
 
   def test_rename_on_different_filesystem
     if File.directory?('/Volumes/MyBook')
-      file = ElFinder::Pathname.new_with_root(@vroot, 'old.txt')
-      FileUtils.touch(file)
+      file = ElFinder::Pathname.new(@vroot, 'old.txt')
+      file.touch
       assert_equal true, File.exist?(File.join(@vroot, 'old.txt'))
-      file.rename('/Volumes/MyBook/elfinder.rename.test.safe.to.delete')
+
+      File.symlink('/Volumes/MyBook', File.join(@vroot, 'mybook'))
+      assert_equal true, File.symlink?(File.join(@vroot, 'mybook'))
+      assert_equal '/Volumes/MyBook', File.readlink(File.join(@vroot, 'mybook'))
+
+      file.rename('mybook/elfinder.rename.test.safe.to.delete')
       assert_equal false, File.exist?(File.join(@vroot, 'old.txt'))
       assert_equal true, File.exist?('/Volumes/MyBook/elfinder.rename.test.safe.to.delete')
       file.unlink
       assert_equal false, File.exist?('/Volumes/MyBook/elfinder.rename.test.safe.to.delete')
     end
-  end
-
-  ################################################################################
-
-  def test_basename_without_extension
-    file = ElFinder::Pathname.new_with_root(@vroot, 'foo.txt')
-    assert_respond_to file, :basename_without_extension
-    assert_equal 'foo', file.basename_without_extension.to_s
-  end
-
-  def test_unique_on_create
-    file = ElFinder::Pathname.new_with_root(@vroot, 'foo.txt')
-    assert_equal 'foo.txt', file.unique.basename.to_s
-  end
-
-  def test_unique_conflict
-    file = ElFinder::Pathname.new_with_root(@vroot, 'pjkh.png')
-    assert_equal 'pjkh 1.png', file.unique.basename.to_s
-  end
-
-  def test_unique_conflict_twice
-    FileUtils.touch ElFinder::Pathname.new_with_root(@vroot, 'pjkh 1.png')
-    file = ElFinder::Pathname.new_with_root(@vroot, 'pjkh.png')
-    assert_equal 'pjkh 2.png', file.unique.basename.to_s
-  end
-
-  def test_relative_to_method
-    assert_equal "", ElFinder::Pathname.new_with_root(@vroot).relative_to(::Pathname.new(@vroot)).to_s
-    assert_equal "foo.txt", ElFinder::Pathname.new_with_root(@vroot, 'foo.txt').relative_to(::Pathname.new(@vroot)).to_s
-    assert_equal "foo/bar.txt", ElFinder::Pathname.new_with_root(@vroot, 'foo/bar.txt').relative_to(::Pathname.new(@vroot)).to_s
-  end
-
-  def test_class_type
-    assert_kind_of ElFinder::Pathname, ElFinder::Pathname.new_with_root(@vroot, 'foo')
-    assert_kind_of ElFinder::Pathname, ElFinder::Pathname.new_with_root(@vroot, 'foo') + 'bar'
-    assert_kind_of ElFinder::Pathname, ElFinder::Pathname.new_with_root(@vroot, 'foo').join('bar')
   end
 
 end
